aa0aa344e123ebce799c909e832d2ed395087f68
mikig <miki.gagyi@gmail.com>
2023-03-04 14:23:04 +0100

    Generate ArticlesController

--- SingleDailyBugle/Models/Article.cs
+++ SingleDailyBugle/Models/Article.cs
@@ -1,6 +1,20 @@
-﻿namespace SingleDailyBugle.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SingleDailyBugle.Models
 {
     public class Article
     {
+        public int Id { get; set; }
+        [Required]
+        public string Author { get; set; } =string.Empty;
+        [Required]
+        public string Title { get; set; } = string.Empty;
+        [Required]
+        public string Synopsis { get; set; } = string.Empty;
+        [Required]
+        public string Body { get; set; } = string.Empty;
+        public DateTime CreatedAt { get; set; }
+        public DateTime ModifiedAt { get; set; }
+        public bool IsDeleted { get; set; }
     }
 }
