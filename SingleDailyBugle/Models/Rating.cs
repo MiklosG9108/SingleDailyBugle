ff4a32d7b1d44287cf80ace88017afc47917f98b
mikig <miki.gagyi@gmail.com>
2023-03-05 18:45:33 +0100

    Create Rating entity

---
+++ SingleDailyBugle/Models/Rating.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SingleDailyBugle.Models
+{
+    public class Rating
+    {
+        public int Id { get; set; }
+        
+        [Required, Range(0, 5)]
+        public int Value { get; set; }
+        public int ArticleId { get; set; }
+        public Article Article { get; set; } = default!;
+    }
+}
