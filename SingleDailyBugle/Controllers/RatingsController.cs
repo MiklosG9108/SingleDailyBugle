07436035ff4b230530af34bb7bf97a1f92c7ba7b
mikig <miki.gagyi@gmail.com>
2023-03-05 19:12:43 +0100

    Create Post method for commenting

--- SingleDailyBugle/Controllers/RatingsController.cs
+++ SingleDailyBugle/Controllers/RatingsController.cs
@@ -6,6 +6,8 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using SingleDailyBugle.Models;
+using SingleDailyBugle.Models.DTOs;
+using SingleDailyBugle.Services;
 
 namespace SingleDailyBugle.Controllers
 {
@@ -13,111 +15,21 @@ namespace SingleDailyBugle.Controllers
     [ApiController]
     public class RatingsController : ControllerBase
     {
-        private readonly ApplicationDbContext _context;
+        private readonly IRatingService _ratingService;
 
-        public RatingsController(ApplicationDbContext context)
+        public RatingsController(IRatingService ratingService)
         {
-            _context = context;
+            _ratingService = ratingService;
         }
 
-        // GET: api/Ratings
-        [HttpGet]
-        public async Task<ActionResult<IEnumerable<Rating>>> GetRatings()
+        
+        [HttpPost(nameof(PostRating))]
+        public async Task<ActionResult> PostRating(int articleId, RatingInputForm rating)
         {
-          if (_context.Ratings == null)
-          {
-              return NotFound();
-          }
-            return await _context.Ratings.ToListAsync();
+            await _ratingService.CreateRatingAsync(articleId, rating);
+
+            return Ok();
         }
 
-        // GET: api/Ratings/5
-        [HttpGet("{id}")]
-        public async Task<ActionResult<Rating>> GetRating(int id)
-        {
-          if (_context.Ratings == null)
-          {
-              return NotFound();
-          }
-            var rating = await _context.Ratings.FindAsync(id);
-
-            if (rating == null)
-            {
-                return NotFound();
-            }
-
-            return rating;
-        }
-
-        // PUT: api/Ratings/5
-        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-        [HttpPut("{id}")]
-        public async Task<IActionResult> PutRating(int id, Rating rating)
-        {
-            if (id != rating.Id)
-            {
-                return BadRequest();
-            }
-
-            _context.Entry(rating).State = EntityState.Modified;
-
-            try
-            {
-                await _context.SaveChangesAsync();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!RatingExists(id))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
-            }
-
-            return NoContent();
-        }
-
-        // POST: api/Ratings
-        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-        [HttpPost]
-        public async Task<ActionResult<Rating>> PostRating(Rating rating)
-        {
-          if (_context.Ratings == null)
-          {
-              return Problem("Entity set 'ApplicationDbContext.Ratings'  is null.");
-          }
-            _context.Ratings.Add(rating);
-            await _context.SaveChangesAsync();
-
-            return CreatedAtAction("GetRating", new { id = rating.Id }, rating);
-        }
-
-        // DELETE: api/Ratings/5
-        [HttpDelete("{id}")]
-        public async Task<IActionResult> DeleteRating(int id)
-        {
-            if (_context.Ratings == null)
-            {
-                return NotFound();
-            }
-            var rating = await _context.Ratings.FindAsync(id);
-            if (rating == null)
-            {
-                return NotFound();
-            }
-
-            _context.Ratings.Remove(rating);
-            await _context.SaveChangesAsync();
-
-            return NoContent();
-        }
-
-        private bool RatingExists(int id)
-        {
-            return (_context.Ratings?.Any(e => e.Id == id)).GetValueOrDefault();
-        }
     }
 }
